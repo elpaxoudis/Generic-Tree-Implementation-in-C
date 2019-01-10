86d11839d5d675fc833d02ae9236ae13eff3bfe2
Nikos Giachoudis <ngiachou@uth.gr>
2019-01-10 18:23:45 +0200

    adding Tree parameter in node insertions and removals

--- src/my_tree.h
+++ src/my_tree.h
@@ -10,13 +10,13 @@ typedef TreeNode* Tree;
 
 void initialize_tree(Tree*);
 
-void add_new_leftmost_child(TreeNode*, const void*, size_t);
-void add_new_rightmost_child(TreeNode*, const void*, size_t);
-void add_new_child_at_pos(TreeNode*, int, const void*, size_t);
+void add_new_leftmost_child(Tree*, TreeNode*, const void*, size_t);
+void add_new_rightmost_child(Tree*, TreeNode*, const void*, size_t);
+void add_new_child_at_pos(Tree*, TreeNode*, int, const void*, size_t);
 
-void* remove_leftmost_child(TreeNode*, size_t);
-void* remove_rightmost_child(TreeNode*, size_t);
-void* remove_child_from_pos(TreeNode*, size_t);
+void* remove_leftmost_child(Tree*, TreeNode*, size_t);
+void* remove_rightmost_child(Tree*, TreeNode*, size_t);
+void* remove_child_from_pos(Tree*, TreeNode*, size_t);
 
 void inorder(Tree, size_t, void (*func)(TreeNode*, size_t));
 void preorder(Tree, size_t, void (*func)(TreeNode*, size_t));
